bacbd1731e7e47d30158d936b11ba99d69246778
zhaojingjing95 <zhaojingjing9595@gmail.com>
2022-08-08 15:19:33 +0300

    setup font header footer main navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,17 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import {Container} from 'react-bootstrap'
 
 function App() {
   return (
     <>
-      
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 }
