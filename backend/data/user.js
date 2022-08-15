7cb2d02d79968a5768f5c1b834803316ce760385
zhaojingjing95 <zhaojingjing9595@gmail.com>
2022-08-15 14:58:35 +0300

    add users.js data

---
+++ backend/data/user.js
@@ -0,0 +1,28 @@
+import bcrypt from 'bcryptjs'
+
+const users = [
+  {
+    name: 'Admin user',
+    email: 'admin@exmaple.com',
+    password: bcrypt.hashSync('123456', 10),
+    isAdmin: true,
+  },
+  {
+    name: 'John Joe',
+    email: 'john@exmaple.com',
+    password: bcrypt.hashSync('123456', 10),
+    isAdmin: false,
+  },
+  {
+    name: 'Jane Joe',
+    email: 'jane@exmaple.com',
+    password: bcrypt.hashSync('123456', 10),
+    isAdmin: false,
+  },
+  {
+    name: 'Jingjing Zhao',
+    email: 'jingjing@exmaple.com',
+    password: bcrypt.hashSync('123456', 10),
+    isAdmin: false,
+  },
+];
