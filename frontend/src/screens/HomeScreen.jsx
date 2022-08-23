74c1411974eb4594134bc66e7d2ca22029e6fe9b
zhaojingjing95 <zhaojingjing9595@gmail.com>
2022-08-23 08:26:46 +0300

    bring redux state into homescreen with useDispatch and useSelector

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -3,19 +3,22 @@ import { useState } from 'react';
 import { Col, Row } from 'react-bootstrap';
 import Product from '../components/Product';
 import axios from 'axios';
+import { useDispatch, useSelector } from 'react-redux';
+import { listProducts } from '../actions/productActions';
+import productReducer from '../reducer/productReducer';
 function HomeScreen() {
-  const [ products, setProducts ] = useState([]);
-
+  const dispatch = useDispatch();
+  const productList = useSelector(state => state.productList)
+  const { loading, products, error} = productList
   useEffect(() => { 
-    const fetchProducts = async () => { 
-      const { data } = await axios.get('/api/products');
-      setProducts(data);
-    }
-    fetchProducts();
-  },[])
+    dispatch(listProducts())
+  }, [ dispatch ])
+ 
   return (
     <>
       <h1>Latest Products</h1>
+      {loading && <h2>Loading...</h2>}
+      { error && <h3>error</h3>}
       <Row>
         {products.map((product) => (
           <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
