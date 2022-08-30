79167113729152ac326c1ce8a0a5fc968b481e3e
zhaojingjing95 <zhaojingjing9595@gmail.com>
2022-08-30 16:40:48 +0300

    generate JWT

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,4 +1,5 @@
 import User from '../models/userModel.js';
+import { generateToken } from '../utils/generateToken.js';
 
 // @desc    Auth User & get token
 // @route   POST /api/users/login
@@ -13,7 +14,7 @@ export const authUser = async (req, res, next) => {
               name: user.name,
               email: user.email,
               isAdmin: user.isAdmin,
-              token: '',
+              token: generateToken(user._id),
           });
       } else { 
           res.status(401)
